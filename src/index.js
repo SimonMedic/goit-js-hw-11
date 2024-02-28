f5c50c38e9df9183a564c1ea00bcec2d70c95ecb
SimonMedic <szymonpaluch@hotmail.com>
2024-02-28 01:26:16 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ import simpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const API_KEY = '42599645-d2386b536dbb9a668471d6710';
+const perPage = 40;
+const currentPage = 1;
+
+const searchForm = document.getElementById('search-form');
+const searchInput = document.querySelector('[name="searchQuery"]');
+const loadMoreButton = document.querySelector('.load-more');
+const gallery = document.querySelector('.gallery');
