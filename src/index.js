756502faef9ac409d8f49f239f1548cdb61d0f03
SimonMedic <szymonpaluch@hotmail.com>
2024-02-28 01:15:10 +0100

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const API_KEY = '42599645-d2386b536dbb9a668471d6710';
